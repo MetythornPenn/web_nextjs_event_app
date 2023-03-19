5ac7817332aa469e34058003bfce2eef0b11128d
Metythorn Penn <metythorn@gmail.com>
2023-03-19 13:19:57 +0700

    update homepage

---
+++ pages/events/event1.js
@@ -0,0 +1,6 @@
+const Page = () => {
+    return <h1>Event Page 1</h1>;
+};
+
+export default Page;
+
---
+++ pages/events/index.js
@@ -0,0 +1,14 @@
+
+const Page =() => {
+    return(
+    <div href=''>
+        <img />
+        <h2>Event in London</h2>
+    </div>
+    
+
+    )
+}
+
+export default Page;
+
